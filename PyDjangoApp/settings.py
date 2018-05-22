c59e1b66ea7db821ee5b1e29cb75d7e501f35f9b
ajayj001 <ajay.j001@gmail.com>
2018-05-22 16:12:08 +0200

    Added requirements

--- PyDjangoApp/settings.py
+++ PyDjangoApp/settings.py
@@ -29,8 +29,8 @@ def relative2absolute_path(relative_path):
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'mhjaa2xl+35**bk^948zagn%ghou#vg6a-_+ez8z)*s6o%1ylo'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'mhjaa2xl+35**bk^948zagn%ghou#vg6a-_+ez8z)*s6o%1ylo'
+#SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
